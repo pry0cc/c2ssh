d1b7813b5d7a50c98bb7be79720d7317ec9a1a81
pry0cc <pry0cc@protonmail.com>
2018-03-20 19:50:41 +0000

    Added files

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM ruby:2.1-onbuild
+EXPOSE 8080
+CMD ["./c2ssh.rb"]
---
+++ c2ssh.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/env ruby
+#
+require 'rubygems'
+require 'json'
+require 'sinatra'
+
+set :environment, :production
+set :port, 8080
+
+master_key = "a187904d1b0023a4d2fbdcd8483170b5021003"
+
+last_port = 2000
+
+def create_db()
+	settings = {"hosts"=>{}}
+	f = File.open("hosts.json", "w")
+	f.write(JSON.generate(settings))
+	f.close
+end
+
+def get_hosts()
+	return JSON.parse(File.open("hosts.json", "r").read())
+end
+
+def add_host(host, port)
+	settings = get_hosts()
+	settings["hosts"][host] = port
+	settingsf = File.open("hosts.json", "w")
+	settingsf.write(JSON.generate(settings))
+	settingsf.close
+end
+
+
+if File.file?("hosts.json")
+	# do nothing
+else
+	puts "hosts.json doesn't exist, creating it..."
+	create_db()
+end
+
+get '/' do
+	return "<head>404</head>"
+end
+
+get '/robots.txt' do
+	return "disallow: /admin"
+end
+
+get '/admin' do
+	return "Sorry bro, I was trolling. There is no admin... Nice try. <!-- STOP TRYNA HACK !-->"
+end
+
+get '/api' do
+	if params["authkey"]
+		if params["authkey"] == master_key
+			if params["action"]
+				if params["action"] == "add"
+					if get_hosts()["hosts"][request.ip]
+						return get_hosts()["hosts"][request.ip.to_s].to_s
+					else
+						add_host(request.ip.to_s, last_port)
+						last_port += 1
+						return get_hosts()["hosts"][request.ip.to_s].to_s
+					end
+				end
+			end
+		else
+			return "Authentication denied"
+		end
+	else
+		return "Not Authenenticated"
+	end
+end
+
