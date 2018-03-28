8435a32208488aa45bfefe9ad1bb334687ca4cc1
pry0cc <pry0cc@protonmail.com>
2018-03-28 17:20:43 +0000

    Added improved hosts2ansible

--- hosts2ansible.rb
+++ hosts2ansible.rb
@@ -3,13 +3,22 @@
 require 'json'
 
 def get_hosts()
-	return JSON.parse(File.open("hosts.json", "r").read())
+	return JSON.parse(File.open("hosts.test", "r").read())
 end
 
 hosts = get_hosts()
 
-hostsfile = File.open("hosts", "w")
+hostsfile = File.open("ssh_config", "w")
 hosts["hosts"].each do |host|
-	hostsfile.write("127.0.0.1:"+host[1].to_s+"\n")
+	hostsfile.write("Host " + host[0]+"\n")
+	hostsfile.write("    Hostname 127.0.0.1\n")
+	hostsfile.write("    Port " + host[1].to_s+"\n")
+	hostsfile.write("    User root\n\n")
 end
 hostsfile.close
+
+anshosts = File.open("hosts", "w")
+hosts["hosts"].each do |host|
+	anshosts.write(host[0]+"\n")
+end
+anshosts.close
